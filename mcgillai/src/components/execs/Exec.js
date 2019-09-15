b12be57c0f027150988a23bab9f79f4d124e12a7
rosieyzh <rosieyzh@gmail.com>
2019-09-15 09:18:29 -0400

    Fixed exec page

--- mcgillai/src/components/execs/Exec.js
+++ mcgillai/src/components/execs/Exec.js
@@ -21,31 +21,52 @@ const Background = styled('div')`
 		padding: 5;
 		margin: 0;
 	}
+	padding: 40px;
+`;
+
+const ImageContainer = styled('div')`
+	width: 33.3%;
+	display: inline-block;
+	vertical-align:top;
+	padding: 30px;
+
+	@media screen and (max-width: 900px) {
+    	width: 50%;
+	}
+
+	@media screen and (max-width: 767px) {
+    	width: 100%;
+	}
+`;
+
+const Image = styled('img')`
+	height: 215px;
+	width: 215px;
+	display: block;
+	margin-left: auto;
+	margin-right: auto;
+`
+
+const Text = styled('p')`
+	padding: 10px;
+	text-align: center;
+	font-size: 16px;
 `;
 
 const Exec = () => (
 	<Background>
-		<Container>
 			<HeadingUnderline> Our Team </HeadingUnderline>
-			<Row>
-				<Col sm={4}>
-					{DATA.map((data, index) => {
-						return (<div>
-							<div>
-								<img src={data.photo}
-									className="center"
-									height="215"
-									width="215"></img>
-								<h2>{data.name}</h2>
-								<h3>{data.position}</h3>
-								<p align="center">{data.bio}</p>
-								Ï		</div>
-						</div>)
-					})}
-				</Col>
-
-			</Row>
-		</Container>
+			
+			{DATA.map((data, index) => {
+				return (<ImageContainer>
+						<Image src={data.photo}
+							className="center"
+							/>
+						<h2>{data.name}</h2>
+						<h3>{data.position}</h3>
+						<Text>{data.bio}</Text>
+				</ImageContainer>)
+			})}
 	</Background>
 );
 export default Exec;
