97a7cfd341e969e2787c2cfe2650ff1e2c5ced34
cclin130 <cheng.lin103@gmail.com>
2020-05-31 11:00:23 -0400

    mcgill AI resources and exec bios

--- mcgillai/src/components/execs/exec-profile.js
+++ mcgillai/src/components/execs/exec-profile.js
@@ -1,138 +1,138 @@
 export const DATA = [
 	{
-		"photo": "/exec-img/hisham.png",
-		"name": "Hisham Hawara",
-		"position": "Co-President",
-		"bio": "Hisham is a final year Software Engineering student. He is interested in computer vision and pattern recognition.",
-		"linkedin": "https://www.linkedin.com/in/hisham-hawara/",
-		"email": ""
-	},
-	{
-		"photo": "/exec-img/jenny.png",
-		"name": "Jenny Long",
-		"position": "Co-President",
-		"bio": "Jenny is a final year Computer Science and Physics student. She is passionate about physics and newly developed interests in graph-based and statistical natural language processing. She loves German.",
-		"linkedin": "https://www.linkedin.com/in/jenny-xly-long/",
-		"email": ""
-	},
-	{
-		"photo": '/exec-img/ketan.png',
-		"name": "Ketan Rampurkar",
-		"position": "VP External",
-		"bio": "Ketan is a third year Software Engineering student with an interest in combining finance and tech to make some impactful software projects.",
-		"linkedin": "https://www.linkedin.com/in/ketan-rampurkar/",
-		"email": ""
-	},
-	{
-		"photo": '/exec-img/claudia.png',
-		"name": "Claudia Leung",
-		"position": "VP Communications",
-		"bio": "Claudia is a final year Psychology and Political Science student with interests in neuroscience, sustainability, and front-end development. When she’s not committing neural pathways to memory, you’ll find her enjoying some BEvERages and playing with p5.js.",
-		"linkedin": "https://www.linkedin.com/in/claudiahleung/",
+		"photo": '/exec-img/cheng.png',
+		"name": "Cheng Lin",
+		"position": "President",
+		"bio": "Cheng is in her fourth year of electrical engineering. She's interested in cool applications of ML and ways of realizing these applications that benefit everyone. She can often be found figure skating, climbing, or listening to music.",
+		"linkedin": "https://www.linkedin.com/in/cheng-lin",
 		"email": ""
 	},
 	{
 		"photo": '/exec-img/rosie.png',
 		"name": "Rosie Zhao",
-		"position": "VP Communications",
-		"bio": "Rosie is a second year Mathematics and Computer Science student with interests in natural language processing and machine learning mathematical theory. She has a love-hate relationship with React and does graphic design in her free time.",
+		"position": "VP Internal",
+		"bio": "Rosie is a third year Mathematics and Computer Science student with interests in natural language processing and machine learning mathematical theory. She has a love-hate relationship with React and does graphic design in her free time.",
 		"linkedin": "https://www.linkedin.com/in/rosieyzh",
 		"email": ""
 	},
 	{
 		"photo": '/exec-img/philip.png',
 		"name": "Philip Tam",
-		"position": "VP Events",
-		"bio": "Philip is a second year Computer Science student with interests in natural language processing, and its applications with automated speech recognition. Rumors say he can talk for days.",
+		"position": "VP External",
+		"bio": "Philip is a third year Computer Science student with interests in natural language processing, and its applications with automated speech recognition. Rumors say he can talk for days.",
 		"linkedin": "https://www.linkedin.com/in/philiptamcode/",
 		"email": ""
 	},
 	{
-		"photo": '/exec-img/etienne.png',
-		"name": "Etienne Denis",
+		"photo": '/exec-img/raph.png',
+		"name": "Raphaëlle Tseng",
+		"position": "VP Communications",
+		"bio": "Raph is a third year Maths and Comp Sci student, keen to apply AI in a way that has a positive social and environmental impact on the world. She likes the beach, a good podcast, and chocolate. Her friends think her spirit animal is a cat. ",
+		"linkedin": "https://www.linkedin.com/in/raphaëlletseng/",
+		"email": ""
+	},
+	{
+		"photo": '/exec-img/teresa.png',
+		"name": "Teresa Altamirano Mayoral",
 		"position": "VP Events",
-		"bio": "Etienne is a final year Computer Science interested in graph representation and reinforcement learning. He was raised in a French household deep in the state of Texas (Austin!). When he is not doing research, he spends his time reading, playing tennis and biking.",
-		"linkedin": "https://www.linkedin.com/in/egdenis/",
+		"bio": "Teresa is a third year Computer Science student with a minor in Psychology. She is interested in the intersection between technology and psychology and how each discipline contributes in some ways to the other. When she is not coding, she is either organizing events, playing the guitar or devouring a fantasy book.",
+		"linkedin": "https://www.linkedin.com/in/teresa-altamirano-mayoral-993967118/?locale=en_US",
+		"email": ""
+	},
+	{
+		"photo": '/exec-img/yulu.png',
+		"name": "Yu Lu Liu",
+		"position": "VP Communications",
+		"bio": "Yu Lu is a second year Stats and Comp Sci student. She is interested in applications of ML, especially in the field of linguistics and of earth sciences. She also loves gardening and watching nature documentaries.",
+		"linkedin": "https://www.linkedin.com/in/yu-lu-liu-5a66861a2/",
+		"email": ""
+	},
+	{
+		"photo": '/exec-img/parsa.png',
+		"name": "Parsa Yadollahi",
+		"position": "VP Finance",
+		"bio": "Parsa is a third year Computer Science student interested in software engineering and quantitative finance. A lover of Toronto and Drake, he embodies the spirit of Trottier's 3rd floor where he and his trademark biceps roam.",
+		"linkedin": "https://www.linkedin.com/in/parsa-yadollahi/",
+		"email": ""
+	},
+	{
+		"photo": '/exec-img/jad.png',
+		"name": "Jad Hamdan",
+		"position": "Academic Lecturer",
+		"bio": "Jad Is a third Year Honours Computer Science and Math student with interest in ML theory and Math. If you have any questions ranging from Computer Science to how to make your rap flow better after chugging half a cup of ~water~, he’d be happy to help. He will be passing that raw knowledge during the MAIS bootcamp lectures. He also goes by the name Senpai.",
+		"linkedin": "https://linkedin.com/in/jad-hamdan/",
+		"email": ""
+	},
+	{
+		"photo": '/exec-img/KC.png',
+		"name": "KC Tsiolis",
+		"position": "Academic Lecturer",
+		"bio": "KC is a third year Mathematics and Computer Science student. During the last two summers, he conducted NLP research at Mila under the supervision of Prof. Jackie Cheung. When not studying, he loves watching, playing, and talking about sports.",
+		"linkedin": "https://linkedin.com/in/kctsiolis/",
+		"email": ""
+	},
+	{
+		"photo": '/exec-img/zedian.png',
+		"name": "Zedian Xiao",
+		"position": "Technical Project Manager",
+		"bio": "Zedian is a third year Mathematics student with minors in Computer Science and Finance. He is passionate about representation learning for natural language processing. In his free time, he enjoys running and exploring new food.",
+		"linkedin": "https://www.linkedin.com/in/zedian-xiao/",
+		"email": ""
+	},
+	{
+		"photo": '/exec-img/ying.png',
+		"name": "Ying Wang",
+		"position": "Technical Project Manager",
+		"bio": "Ying is a fourth year Statistics and Computer Science student with a minor in Finance. She is interested in data science and quantitative finance. With a love for nature, she takes photos of stunning sunsets every day :D",
+		"linkedin": "https://www.linkedin.com/in/ying-wang-90611714a/",
+		"email": ""
+	},
+	{
+		"photo": '/exec-img/william.png',
+		"name": "William Zhang",
+		"position": "Technical Project Manager",
+		"bio": "William is a third year software engineering student very interested in the applications of machine learning in finance.",
+		"linkedin": "https://www.linkedin.com/in/williamykzhang/",
+		"email": ""
+	},
+	{
+		"photo": '/exec-img/youssef.png',
+		"name": "Mohamed Youssef Bouaouina",
+		"position": "Technical Project Manager",
+		"bio": "Mohamed Youssef is a fourth year electrical engineering student. He is interested in systems control and software engineering. Despite not being very skilled at it, he lives and breathes basketball.",
+		"linkedin": "https://www.linkedin.com/in/mohamedyoussefb/",
+		"email": ""
+	},
+	{
+		"photo": '/exec-img/ketan.png',
+		"name": "Ketan Rampurkar",
+		"position": "Senior Advisor",
+		"bio": "Ketan is a third year Software Engineering student with an interest in combining finance and tech to make some impactful software projects.",
+		"linkedin": "http://linkedin.com/in/ketan-rampurkar/",
+		"email": ""
+	},
+	{
+		"photo": '/exec-img/hisham.png',
+		"name": "Hisham Hawara",
+		"position": "Senior Advisor",
+		"bio": "Hisham has a degree in Software Engineering. He is interested in computer vision and pattern recognition.",
+		"linkedin": "http://linkedin.com/in/hisham-hawara/",
 		"email": ""
 	},
 	{
 		"photo": '/exec-img/aakarsh.png',
 		"name": "Aakarsh Shekhar",
-		"position": "VP Finance",
+		"position": "Senior Advisor",
 		"bio": "Aakarsh is a third year Software Engineering student. He is interested in real-world applications of machine learning primarily in the field of pattern recognition.",
 		"linkedin": "http://linkedin.com/in/aakarshshekhar/",
 		"email": ""
 	},
 	{
-		"photo": '/exec-img/ali.png',
-		"name": "Ali Shobeiri",
-		"position": "Academic Lecturer",
-		"bio": "Ali is a good looking individual who is in his final year of Electrical Engineering. He was born in Iran and will look to impress you with his lecturing during the MAIS bootcamp.",
-		"linkedin": "https://linkedin.com/in/ali-shobeiri/",
-		"email": ""
-	},
-	{
 		"photo": '/exec-img/antonios.png',
 		"name": "Antonios Valkanas",
-		"position": "Academic Lecturer",
-		"bio": "Hailing from Athens, Greece, this man knows his way around a neural network. He will be lecturing his heart out during the MAIS bootcamp. He is also a first year graduate student interested in graph neural networks.",
+		"position": "Senior Advisor",
+		"bio": "Antonios is a second year Master’s student in Electrical Engineering. His research focuses on deep learning for graphs. He likes teaching AI to undergrads, travelling to places where he can’t communicate with the locals and sports.",
 		"linkedin": "https://www.linkedin.com/in/antonios-valkanas/",
 		"email": ""
 	},
-	{
-		"photo": '/exec-img/cheng.png',
-		"name": "Cheng Lin",
-		"position": "Technical Project Manager",
-		"bio": "Cheng is in her third year of electrical engineering. She's interested in machine learning applications in traditionally non-technical fields. In her free time, she figure skates and listens to too much music.",
-		"linkedin": "https://www.linkedin.com/in/cheng-lin",
-		"email": ""
-	},
-	{
-		"photo": '/exec-img/diego.png',
-		"name": "Diego Lopez",
-		"position": "Technical Project Manager",
-		"bio": "Diego is a second year Math and Computer Science student. He is interested in the mathematical foundations of machine learning. On rare occasions, he can be found wearing something other than a blue shirt.",
-		"linkedin": "https://www.linkedin.com/in/lopdie101/",
-		"email": ""
-	},
-	{
-		"photo": '/exec-img/li.png',
-		"name": "Li Zhang",
-		"position": "Technical Project Manager",
-		"bio": "Li is a final year Software Engineering student. She's interested in various fields of machine learning, but computer vision is one of her favourite. Her specialty is to learn while building, and to “make things work”.",
-		"linkedin": "https://www.linkedin.com/in/litigre/",
-		"email": ""
-	},
-	{
-		"photo": '/exec-img/rick.png',
-		"name": "Rick Wu",
-		"position": "Technical Project Manager",
-		"bio": "Rick is a second year Master of Electrical Engineering student. His research interests concern computer vision and deep learning, primarily focusing on video summarization using multimodal learning.",
-		"linkedin": "https://www.linkedin.com/in/rick-wu/",
-		"email": ""
-	},
-	{
-		"photo": '/exec-img/david.png',
-		"name": "David Tao",
-		"position": "Senior Advisor",
-		"bio": "David is a final year Honors Computer Science student. His interests currently reside in model-based reinforcement learning. He’s also really really old.",
-		"linkedin": "https://www.linkedin.com/in/taodav/",
-		"email": ""
-	},
-	{
-		"photo": '/exec-img/isaac.png',
-		"name": "Isaac Chan",
-		"position": "Senior Advisor",
-		"bio": "Isaac is a final year electrical engineering student. He is interested in understanding the mechanics of deep reinforcement learning in the context of few-shot learning and HRL.",
-		"linkedin": "https://www.linkedin.com/in/chan-isaac-ezw/",
-		"email": ""
-	},
-	{
-		"photo": '/exec-img/nabil.png',
-		"name": "Nabil Chowdhury",
-		"position": "Senior Advisor",
-		"bio": "Nabil is a final year Software Engineering student. He enjoys backend development and solving algorithms, although you may sometimes hear him say \"border-radius\" and \"margin.\"",
-		"linkedin": "https://www.linkedin.com/in/nabil-chowdhury/",
-		"email": ""
-	}
 ]
--- mcgillai/src/components/mtlai/resources2.js
+++ mcgillai/src/components/mtlai/resources2.js
@@ -5,13 +5,6 @@ import {
 	ImageBackground
  } from '../commonstyles'
 
- import Mais202 from './activities-img/mais202.jpg';
- import ImplementAI from './activities-img/implementai.jpg';
- import Workshops from './activities-img/workshops.jpg';
- import WomenInAI from './activities-img/womeninai.jpg';
- import ResearchTalks from './activities-img/researchtalks.jpg';
- import CompanyCrawl from './activities-img/companycrawl.JPG';
-
 const Container = styled('div')`
 	margin: auto;
   	position: relative;
@@ -24,7 +17,7 @@ const ImageGrid = styled('div')`
 	font-size: 0;
 `;
 
-const ImageCell = styled('div')`
+const ImageCell = styled('a')`
 	display: inline-block;
 	vertical-align: top;
 	width: 33.33%;
@@ -66,20 +59,20 @@ const ImageBounds = styled('div')`
 	right: 0;
 	bottom: 0;
 	left: 0;
-	opacity: 0;
+	opacity: 1;
 	transition: opacity 500ms;
 	background: ${props => props.red ? 'linear-gradient(to bottom,#722D60, #A81245)' : 'linear-gradient(to bottom,#6E2F62, #003399)'};
 `;
 
 const ImageText = styled('div')`
 	position: absolute;
-	bottom: 0;
-	left: 0;
-	right: 0;
-	padding: 20px;
+	left: 50%;
+	top: 50%;
+	transform: translate(-50%, -50%);
+	width: 90%;
 	color: white;
 	text-align: center;
-	opacity: 0;
+	opacity: 1;
 	transition: opacity 500ms;
 
 	> h2 {
@@ -133,69 +126,72 @@ const ImageText = styled('div')`
 `;
 
 
-class Activities extends React.Component {
+class Resources extends React.Component {
 	render () {
 		return (
 			<Container>
 				<ImageGrid>
 
-					<ImageCell>
-						<ImageBackground className="Background" style={{backgroundImage: `url(${Mais202})`}}>  </ImageBackground>
+					<ImageCell href = "https://www.mcgill.ca/cscds/" target='_blank' rel="noopener noreferrer" className='link'>
 						<ImageBounds red={true} className="Bounds">
 						</ImageBounds>
 						<ImageText className="Text">
-							<h2>MAIS202 Bootcamp</h2>
-							<h3>MAIS 202 is a ten-week bootcamp that teaches lower year undergraduates ML fundamentals. With over 60 bootcamp alumni, our Fall 2020 iteration will welcome our fourth cohort of students.</h3>
+							<h2>CSCDS (Centre for Social and Cultural Data Science)</h2>
+							<h3>CSCDS is a multi-disciplinary community of scholars who apply data science to research in the humanities and social sciences through training, networking and support.</h3>
 						</ImageText>
 					</ImageCell>
 
-					<ImageCell>
-						<ImageBackground className="Background" style={{backgroundImage: `url(${ImplementAI})`}}> </ImageBackground>
+					<ImageCell href = "http://networkdynamics.org/" target='_blank' rel="noopener noreferrer" className='link'>
 						<ImageBounds className="Bounds">
 						</ImageBounds>
 						<ImageText className="Text">
-							<h2>ImplementAI</h2>
-							<h3>Our annual hackathon, ImplementAI, brings together 150 students across Eastern Canada to design and innovate modern AI solutions.</h3>
+							<h2>Network Dynamics Lab</h2>
+							<h3>The Network Dynamics Lab is led by Prof. Derek Ruths and aims to measure and model large-scale human behavior, such as those found within social platforms, NYT bestsellers, human communities, political parties, and ancient civilizations.</h3>
 						</ImageText>
 					</ImageCell>
 
-					<ImageCell>
-						<ImageBackground className="Background" style={{backgroundImage: `url(${CompanyCrawl})`}}> </ImageBackground>
+					<ImageCell href = "https://txtlab.org/" target='_blank' rel="noopener noreferrer" className='link'>
 						<ImageBounds className="Bounds">
 						</ImageBounds>
 						<ImageText className="Text">
-							<h2>Company Crawl</h2>
-							<h3>Each fall, we invite over 30 talented individuals to tour some of the hottest AI companies in Montreal, including Automat, StrategiAI, Google, and Microsoft. </h3>
+							<h2>.txtLAB @ McGill</h2>
+							<h3>.txtLAB applies statistical and machine learning techniques to the study of culture, with a focus on cultural inequality.</h3>
 						</ImageText>
 					</ImageCell>
 
-					<ImageCell>
-						<ImageBackground className="Background" style={{backgroundImage: `url(${Workshops})`}}> </ImageBackground>
+					<ImageCell href = "http://www.aaronerlich.com/demotip-laboratory" target='_blank' rel="noopener noreferrer" className='link'>
+						<ImageBounds className="Bounds">
+						</ImageBounds>
+						<ImageText className="Text">
+							<h2>demoTIP</h2>
+							<h3>The DemoTIP laboratory applies state of the art research methods, such as machine learning, to bring answers to empirical problems in political science.</h3>
+						</ImageText>
+					</ImageCell>
+
+					<ImageCell href = "https://www.policyanddatascience.org/" target='_blank' rel="noopener noreferrer" className='link'>
 						<ImageBounds red={true} className="Bounds">
 						</ImageBounds>
 						<ImageText className="Text">
-							<h2>Workshops</h2>
-							<h3>Our technical project managers provide workshops each term teaching applied topics in artificial intelligence and machine learning. Our 2019-2020 workshop series covered topics such as image classification, OpenCV, and building a chatbot.</h3>
+							<h2>Policy and Data Science Summer Program</h2>
+							<h3>Held by CSCDS and the Max Bell School of Public Policy, the summer program trains students from non-technical backgrounds in data science. Students finish the program by applying their skills in a policy internship.</h3>
 						</ImageText>
 					</ImageCell>
 
-					<ImageCell>
-						<ImageBackground className="Background" style={{backgroundImage: `url(${WomenInAI})`}}> </ImageBackground>
+					<ImageCell href = "https://www.facebook.com/McGillNeurotech/" target='_blank' rel="noopener noreferrer" className='link'>
 						<ImageBounds className="Bounds">
 						</ImageBounds>
 						<ImageText className="Text">
-							<h2>Women in AI Panel</h2>
-							<h3>To celebrate International Women's Day, we invited Joelle Pineau, Layla El Asri, Audrey Durand, and Negar Rostamzadeh to talk about their notable achievements and share their career progression in AI. </h3>
+							<h2>McGill Neuro Tech</h2>
+							<h3>A student-run design team that participates in the NeuroTechX Student Clubs Competition each year, having won each year. Projects usually involve ML applications in neurotechnology.</h3>
 						</ImageText>
 					</ImageCell>
 
-					<ImageCell>
-						<ImageBackground className="Background" style={{backgroundImage: `url(${ResearchTalks})`}}> </ImageBackground>
+					<ImageCell href = "http://www.ece.mcgill.ca/~aemad2/" target='_blank' rel="noopener noreferrer" className='link'>
 						<ImageBounds red={true} className="Bounds">
 						</ImageBounds>
 						<ImageText className="Text">
-							<h2>Research Talks</h2>
-							<h3>A series of informal presentations for students to engage with state-of-the-art research pursued by academic and industry researchers in machine learning. </h3>
+							<h2>McGill Computational Biology and Artificial Intelligence Lab</h2>
+							<h3>A cool opportunity for anyone keen on AI and life sciences!</h3>
 						</ImageText>
 					</ImageCell>
 				</ImageGrid>
@@ -204,4 +200,4 @@ class Activities extends React.Component {
 	}
 }
 
-export default Activities
+export default Resources
