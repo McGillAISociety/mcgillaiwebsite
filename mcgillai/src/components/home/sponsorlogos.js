82b331f549bff7e42283cb39d0f446e11374a98c
rosieyzh <rosieyzh@gmail.com>
2020-09-06 12:00:45 -0400

    updated sponsors

--- mcgillai/src/components/home/sponsorlogos.js
+++ mcgillai/src/components/home/sponsorlogos.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-import NationalBankLogo from './sponsors-img/nationalbank.png';
-import CaeLogo from './sponsors-img/cae.png';
-import CoveoLogo from './sponsors-img/coveo.png';
-import HatchLogo from './sponsors-img/hatch.png';
-import SamasourceLogo from './sponsors-img/samasource.png';
 import wrnchLogo from './sponsors-img/wrnch.png';
+import intactLogo from './sponsors-img/intact.png';
+import nvidiaLogo from './sponsors-img/nvidia.png';
 
 const LogoRow = styled('div')`
   display: flex;
@@ -54,35 +51,24 @@ export default () => (
   <React.Fragment>
     <LogoRow>
       <Logo
-        alt="NationalBank"
-        src={NationalBankLogo}
-        href="https://jobs.nbc.ca/students-internships"
+        alt="wrnch"
+        src={wrnchLogo}
+        href="https://wrnch.ai/careers/"
       />
     </LogoRow>
     <LogoRow>
       <Logo
-        alt="Cae"
-        src={CaeLogo}
-        href="https://www.cae.com/careers/job-categories/students-and-new-graduates/"
+        alt="intact"
+        src={intactLogo}
+        href="https://careers.intact.ca/ca/en/"
       />
+     </LogoRow>
+     <LogoRow>
       <Logo
-        alt="Coveo"
-        src={CoveoLogo}
-        href="https://www.coveo.com/en/company/careers"
+        alt="nvidia"
+        src={nvidiaLogo}
+        href="https://www.nvidia.com/en-us/about-nvidia/careers/"
       />
-      <Logo
-        alt="Hatch"
-        src={HatchLogo}
-        href="https://jobs.hatch.com/go/Student-&-New-Graduate-Jobs/2565100/"
-      />
-    </LogoRow>
-    <LogoRow>
-      <Logo
-        alt="Samasource"
-        src={SamasourceLogo}
-        href="https://www.samasource.com/careers"
-      />
-      <Logo alt="wrnch" src={wrnchLogo} href="https://wrnch.ai/careers/" />
     </LogoRow>
   </React.Fragment>
 );
