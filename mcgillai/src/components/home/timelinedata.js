96f29fc9eb71d574e5ac40d9e0cc4f7c007e78f5
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2021-06-29 19:08:35 -0400

    remove old upcoming events

--- mcgillai/src/components/home/timelinedata.js
+++ mcgillai/src/components/home/timelinedata.js
@@ -1,14 +1,1 @@
-export const TimelineData = [
-    {
-        title: 'MAIS 202 Winter 2021 Applications',
-        time: 'January 15 | 11:59 PM EST',
-        place: 'Online!',
-        category: '/schedule-icons/mais202.svg',
-    },
-    {
-        title: 'McGill AI Learnathon!',
-        time: 'February 6',
-        place: 'Online!',
-        category: '/schedule-icons/panel.svg',
-    },
-];
+export const TimelineData = [];
