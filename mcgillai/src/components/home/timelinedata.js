727ef11a8b2ab390d978224498e17e98706ef543
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2021-07-09 16:55:37 -0400

    add events for MAIS apps and hackathon

--- mcgillai/src/components/home/timelinedata.js
+++ mcgillai/src/components/home/timelinedata.js
@@ -1,1 +1,14 @@
-export const TimelineData = [];
+export const TimelineData = [
+    {
+        title: 'MAIS 202 Fall 2021 Applications Out',
+        time: 'August (date TBA)',
+        place: 'Location TBA',
+        category: '/schedule-icons/mais202.svg',
+    },
+    {
+        title: 'MAIS Hacks',
+        time: 'October (date TBA)',
+        place: 'Location TBA',
+        category: '/schedule-icons/panel.svg',
+    },
+];
