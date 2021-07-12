ddd43a381b627a6cf430cbff0d2a29d0180e2230
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2021-07-11 22:03:52 -0400

    fix

--- mcgillai/src/components/home/landing.js
+++ mcgillai/src/components/home/landing.js
@@ -68,7 +68,6 @@ const DesktopButtonContainer = styled('div')`
 const MobileButtonContainer = styled('div')`
     width: 100%;
     display: flex;
-    align-items: center;
     justify-content: center;
 `;
 
