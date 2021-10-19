ed34caecf202419e41966225ef208b80e61f9501
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2021-10-19 00:27:32 -0400

    update timeline

--- mcgillai/src/components/home/timelinedata.js
+++ mcgillai/src/components/home/timelinedata.js
@@ -1,18 +1,7 @@
 export const TimelineData = [
     {
-        title: 'MAIS 202 Fall 2021 Applications Due',
-        time: 'September 8th',
-        category: '/schedule-icons/mais202.svg',
-    },
-    {
-        title: 'MAIS Hacks 2021 Applications Due',
-        time: 'Sepetmber 13',
-        category: '/schedule-icons/panel.svg',
-    },
-    {
-        title: 'MAIS Hacks 2021',
-        time: 'October 2-3',
-        place: 'MILA // Online',
-        category: '/schedule-icons/panel.svg',
+        title: 'Workshop 2: ML Model Deployment',
+        time: 'October 28th',
+        category: '/schedule-icons/workshop.svg',
     },
 ];
