430c8db9b84810b14c08880a235ad834b6217fa5
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2021-10-27 20:13:28 -0400

    november events

--- mcgillai/src/components/home/timelinedata.js
+++ mcgillai/src/components/home/timelinedata.js
@@ -1,7 +1,12 @@
 export const TimelineData = [
     {
-        title: 'Workshop 2: ML Model Deployment',
-        time: 'October 28th',
+        title: 'Workshop 3: Weights and Biases',
+        time: 'November 10th',
+        category: '/schedule-icons/workshop.svg',
+    },
+    {
+        title: 'AI Ethics Panel',
+        time: 'November 17th',
         category: '/schedule-icons/workshop.svg',
     },
 ];
