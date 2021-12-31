6e401a6121ceeaae5e026ecd9fc2cccd053f19fa
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2021-12-31 12:26:18 -0500

    mais 202 winter 2022

--- mcgillai/src/components/home/timelinedata.js
+++ mcgillai/src/components/home/timelinedata.js
@@ -1,8 +1,7 @@
 export const TimelineData = [
     {
-        title: 'MAIS 202 Project Fair',
-        time: 'December 2nd, 1-4 PM',
+        title: 'MAIS 202 Applications Due',
+        time: 'January 14th',
         category: '/schedule-icons/mais202.svg',
-        place: 'In person @ Trottier Mezzanine',
     },
 ];
