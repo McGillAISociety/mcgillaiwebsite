28acc24c2804cf77edb3a870f1f91b6cf3b0b7e3
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2022-01-12 10:26:05 -0500

    use new libarry for MAIS202 gallery

--- pages/mais202/index.js
+++ pages/mais202/index.js
@@ -1,61 +1,46 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import Slider from 'react-slick';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
 import styles from '../../styles/pages/mais202.module.scss';
 
 import About from './about.js';
 
 import FAQ from './faq.js';
 
-const Image = styled('img')`
-    width: 600px;
-
-    @media screen and (max-width: 767px) {
-        width: 500px;
-        height: auto;
-    }
-    @media screen and (max-width: 600px) {
-        width: 400px;
-        height: auto;
-    }
-    @media screen and (max-width: 490px) {
-        display: none;
-    }
-`;
-
 export default function MAIS202() {
-    const settings = {
-        autoplay: true,
-        autoplaySpeed: 2000,
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-    };
     return (
         <>
             <h1>MAIS202</h1>
-            <h3>ACCELERATED INTRODUCTION TO MACHINE LEARNING</h3>
-            <p>2021-2022 Offerings: Fall 2021, Winter 2022</p>
-            {/* <SubscribeButton href="https://tiny.cc/mais202-w2020-app" target="_blank">APPLY NOW!</SubscribeButton> */}
-            <Slider {...settings}>
-                <img src="/images/mais202/gallery/1.jpg" />
-
-                <img src="/images/mais202/gallery/2.jpg" />
-
-                <img src="/images/mais202/gallery/3.jpg" />
-
-                <img src="/images/mais202/gallery/4.jpg" />
-
-                <img src="/images/mais202/gallery/5.jpg" />
-
-                <img src="/images/mais202/gallery/6.jpg" />
-            </Slider>
+            <div className="flex-center">
+                <div>
+                    <h3>ACCELERATED INTRODUCTION TO MACHINE LEARNING</h3>
+                    <p>2021-2022 Offerings: Fall 2021, Winter 2022</p>
+                </div>
+                {/* <SubscribeButton href="https://tiny.cc/mais202-w2020-app" target="_blank">APPLY NOW!</SubscribeButton> */}
+                <Carousel
+                    autoPlay
+                    infiniteLoop
+                    stopOnHover={false}
+                    showStatus={false}
+                    showThumbs={false}
+                    interval={1500}
+                    className={styles['carousel']}
+                >
+                    {['1', '2', '3', '4', '5', '6'].map((number) => (
+                        <img
+                            key={number}
+                            src={`/images/mais202/gallery/${number}.jpg`}
+                            alt="MAIS"
+                            className={styles['carousel__img']}
+                        />
+                    ))}
+                </Carousel>
+            </div>
             <div className="flex-center">
                 {['1', '2', '3', '4'].map((number) => (
                     <img
-                        index={number}
+                        key={number}
                         src={`/images/mais202/stats_${number}.svg`}
                         alt="MAIS Statistic"
                         className={styles['stat-img']}
