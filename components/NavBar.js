2d1bb4ae30763737f1efaa04ce18e022508c1c4e
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2022-01-18 21:53:02 -0500

    basic page transitions & some navbar planning

--- components/NavBar.js
+++ components/NavBar.js
@@ -42,15 +42,44 @@ const navOptions = [
 
 function NavBar() {
     const [expanded, setExpanded] = useState(false);
-    // TODO: smooth scroll to top of page when nav-ing
-    const scrollToTop = () => {
-        window.scrollTo({ top: 0 });
-    };
-
     const router = useRouter();
 
     // TODO: cleanup the classname structure maybe
     // TODO: do a background gradient for active links accross navbar? and animate globally between them?
+
+    // TODO: don't display the logo as part of a flexbox, absolute position it instead
+    // then wrap the navLinks in a div that 1) has a left-margin of the same width as the logo and 2) has className mobile-hide
+    // desktop-hide the react-burger-menu burger and use a column flexbox to display navLinks in the menu
+
+    const navLinks = navOptions.map((navOption, index) => {
+        const props = {
+            href: navOption.route,
+            key: index,
+        };
+        const active = router.pathname === `/${navOption.route}`;
+        const item = (
+            <span
+                className={`${styles['nav__item']}
+                    ${styles['nav__item--text']}
+                    ${active ? styles['nav__item--text--active'] : ''}`}
+            >
+                {navOption.title}
+                {navOption.external && (
+                    <HiExternalLink
+                        className={styles['nav__item--icon']}
+                        size={25}
+                    />
+                )}
+            </span>
+        );
+        return navOption.external ? (
+            <a {...props} target="_blank" rel="noopener noreferrer">
+                {item}
+            </a>
+        ) : (
+            <Link {...props}>{item}</Link>
+        );
+    });
 
     return (
         <nav className={`${styles['nav']} flex-center`}>
@@ -79,35 +108,7 @@ function NavBar() {
                     />
                 </div>
             </Link>
-            {navOptions.map((navOption, index) => {
-                const props = {
-                    href: navOption.route,
-                    key: index,
-                };
-                const active = router.pathname === `/${navOption.route}`;
-                const item = (
-                    <span
-                        className={`${styles['nav__item']}
-                            ${styles['nav__item--text']}
-                            ${active ? styles['nav__item--text--active'] : ''}`}
-                    >
-                        {navOption.title}
-                        {navOption.external && (
-                            <HiExternalLink
-                                className={styles['nav__item--icon']}
-                                size={25}
-                            />
-                        )}
-                    </span>
-                );
-                return navOption.external ? (
-                    <a {...props} target="_blank" rel="noopener noreferrer">
-                        {item}
-                    </a>
-                ) : (
-                    <Link {...props}>{item}</Link>
-                );
-            })}
+            {navLinks}
         </nav>
     );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,37 @@ import React from 'react';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import '../styles/app.scss';
+import { motion } from 'framer-motion';
 
-// TODO: animate in the navbar and the component (maybe later try to mimic the multiple section animations from old site)
+// TODO: animate in the navbar?
+// TODO: animate in different page sections individually?
 // TODO: render MetaTags here with different props based on the route? If that doesnt work just render MetaTags differently in each page
 // TODO: render the heading underline here with subtitle based on route-associated title and subtitle fields
-function MAIS({ Component, pageProps }) {
+function MAIS({ Component, pageProps, router }) {
     return (
         <>
             <NavBar />
-            <div className="page">
-                <Component {...pageProps} />
-            </div>
-            <Footer />
+            <motion.div
+                layout
+                key={router.route}
+                initial="initial"
+                animate="animate"
+                variants={{
+                    initial: {
+                        opacity: 0,
+                        y: 30,
+                    },
+                    animate: {
+                        opacity: 1,
+                        y: 0,
+                    },
+                }}
+            >
+                <div className="page">
+                    <Component {...pageProps} />
+                </div>
+                <Footer />
+            </motion.div>
         </>
     );
 }
