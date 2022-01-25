3565c7ed38dc6ff3053e52c7d216a1fada65cfbe
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2022-01-25 14:54:24 -0500

    add a bit of transparency to nav menus

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,16 @@ import { motion } from 'framer-motion';
 // TODO: animate in different page sections individually?
 // TODO: render MetaTags here with different props based on the route? If that doesnt work just render MetaTags differently in each page
 // TODO: render the heading underline here with subtitle based on route-associated title and subtitle fields
+const pageData = [
+    {
+        title: '',
+        subtitle: '',
+        description: '',
+        imagePath: '',
+    },
+    // ...
+];
+
 function MAIS({ Component, pageProps, router }) {
     return (
         <>
@@ -28,7 +38,7 @@ function MAIS({ Component, pageProps, router }) {
                     },
                 }}
             >
-                <div className="page">
+                <div className="section">
                     <Component {...pageProps} />
                 </div>
                 <Footer />
