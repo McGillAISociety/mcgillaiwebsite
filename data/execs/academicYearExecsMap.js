81ab4f32cc05e0d1176c42a4316bf5451f288d5c
Signe Hoel <signe.hoel@gmail.com>
2023-07-30 21:15:02 -0400

    exec updates

--- data/execs/academicYearExecsMap.js
+++ data/execs/academicYearExecsMap.js
@@ -1,3 +1,4 @@
+import execs2023_2024 from './2023-2024.js';
 import execs2022_2023 from './2022-2023.js';
 import execs2021_2022 from './2021-2022.js';
 import execs2020_2021 from './2020-2021.js';
@@ -5,7 +6,8 @@ import execs2019_2020 from './2019-2020.js';
 
 // it's important for this to be more-recent-years=first
 const academicYearExecsMap = new Map();
-academicYearExecsMap.set('currentAcademicYear', '2022-2023');
+academicYearExecsMap.set('currentAcademicYear', '2023-2024');
+academicYearExecsMap.set('2023-2024', execs2023_2024);
 academicYearExecsMap.set('2022-2023', execs2022_2023);
 academicYearExecsMap.set('2021-2022', execs2021_2022);
 academicYearExecsMap.set('2020-2021', execs2020_2021);
