7c4fa89cce8eb82b8c5dacbe9a0b13ebeef5112d
Alexa <90080367+itsAlexa@users.noreply.github.com>
2023-11-20 11:06:48 -0500

    Added Initial Event Component

--- pages/events.js
+++ pages/events.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Image from 'next/image';
+import Event from "../components/Event";
 
 export default function Events() {
     return(
         <>
             <section>
                 <p>Stay up to date with all of our events for the current academic year!</p>
+                <Event></Event>
             </section>
         </>
     );
