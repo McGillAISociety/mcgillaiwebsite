a373859b964c9b671d8947267d51ebc38ec53eef
Alexa <90080367+itsAlexa@users.noreply.github.com>
2023-12-28 15:16:58 -0500

    Added More Event Data + Locations and Images

--- components/Events.js
+++ components/Events.js
@@ -6,10 +6,11 @@ import styles from '../styles/components/Events.module.scss'
 eventData takes the following form:
 [
     {
-        photo: `/images/events/2023-2024/learnathon.png`,
-        name: 'MAIS Learnathon 2024',
-        datetime: 'January _, 2024, 2:00 PM - 5:00 PM'
-        description: ''
+        photo: `/images/events/2023-2024/trivianight23.png`,
+        name: 'AI Trivia Night',
+        datetime: 'October 30th, 2023 from 5:30 PM - 7:00 PM',
+        location: 'EDUC 211'
+        description: 'Get ready for an electrifying event of knowledge...',
     }, ...
 ]
 */
@@ -26,8 +27,9 @@ export default function Events({ eventsData = [] }) {
                         height={400}
                     />
                     <div className={`flex-start ${styles['event-details']}`}>
-                        <h3>{eventData.name}</h3>
+                        <h2>{eventData.name}</h2>
                         <h3>{eventData.datetime}</h3>
+                        <h4>{eventData.location}</h4>
                         <p>{eventData.description}</p>
                     </div>
                 </div>
