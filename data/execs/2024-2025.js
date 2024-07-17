a81fda69e838678a4d3949f4a6af3a49ebb4a064
Melek Deniz Colak <melekdenizcolak21@gmail.com>
2024-07-17 21:12:21 +0300

    updated podcast team for 2024-2025

---
+++ data/execs/2024-2025.js
@@ -0,0 +1,69 @@
+const path = (name) => `/images/team/2024-2025/${name}.png`;
+
+export default [
+    {
+        photo: path('duru'),
+        name: 'Duru Aran',
+        position: 'Co-President',
+        bio: "Duru is a third year student majoring in Math and Computer Science, minoring in Linguistics to really be able to understand AI. Although she only recently became an AI enthusiast, she's most interested in its financial applications, as well as learning how it's developed from the very start. Outside of the library you can find her hanging out with friends, playing the trumpet, or rewatching her comfort shows under a blanket.",
+        linkedin: 'https://www.linkedin.com/in/duru-aran-a39970228',
+    },
+    {
+        photo: path('ada'),
+        name: 'Ada Tur',
+        position: 'Co-President',
+        bio: 'Ada is a third-year CS and Linguistics student, a researcher at Mila, and a 2024 AAAI UC Scholar. ',
+        linkedin: 'https://www.linkedin.com/in/adadtur/',
+    },
+    {
+        photo: path('eya'),
+        name: 'Eya Ibrahim',
+        position: 'VP Events',
+        bio: 'Eya is a third-year student from BC studying computational neuroscience. She hopes to delve into the world of neurotechnology, and BCIs in particular. In her research, she is working on ways in which AI can optimize pattern recognition for brain activity. ',
+        linkedin: 'http://linkedin.com/in/eya-ibrahim-0a130423b',
+    },
+    {
+        photo: path('ramatoulaye'),
+        name: 'Ramatoulaye Balde',
+        position: 'Podcast Producer',
+        bio: "Ramatoulaye is a first-year Computer Engineering student minoring in Applied AI, interestd in AI applications in medicine and the ethical implications of AI's growing role in various fields. She enjoys video games, superheroes, and anime.",
+        linkedin: 'https://www.linkedin.com/in/ramatoulayebalde?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app',
+    },
+    {
+        photo: path('alexandre'),
+        name: 'Alexandre Lamarche',
+        position: 'Podcast Producer',
+        bio: "Alexandre is a second year Software Engineering student with a minor in Cognitive science. Passionate about the intersection of human and artificial intelligence. He enjoys running and classic video games. He is currently an intern at LANDR audio.",
+        linkedin: 'https://www.linkedin.com/in/alexandre-lamarche-in/'
+    },
+    {
+        photo: path('catherinee'),
+        name: 'Catherine Fontaine',
+        position: 'Podcast Producer',
+        bio: "Catherine is a fourth year Math and CS student who loves dancing and traveling. They discovered a new passion for cooking.",
+        linkedin: 'https://www.linkedin.com/in/catherine-f-236796206'
+    },
+    {
+        photo: path('antoine'),
+        name: 'Antoine Paradis',
+        position: 'Podcast Producer',
+        bio: "Antoine is a third-year physics, computer science, and economics student with a passion for interdisciplinary problems. In his spare time, he enjoys golfing and skiing,  with a current interest in perfecting his beer-making skills (talk to me about it!). He is currently exploring the world of quantitative finance at National Bank, and is excited to bring insightful conversations about AI to the general public. ",
+        linkedin: 'https://www.linkedin.com/in/antoineparadis'
+    },
+    {
+        photo: path('veronica'),
+        name: 'Veronica Xia',
+        position: 'Senior Advisor',
+        bio: 'Veronica is a Masters student, focusing on user-centred AI design and human-AI interaction. She also enjoys reading, window shopping around Montreal, and making easily store-bought foods.',
+        linkedin: 'https://www.linkedin.com/in/veronicayx/',
+    },
+    {
+        photo: path('alexandres'),
+        name: 'Alexandre St-Onge',
+        position: 'Senior Advisor',
+        bio: 'Alexandre is a third-year law student interested in AI regulation, privacy law, and time series forecasting. In his spare time, he enjoys reading, playing golf and chess, and discovering new music.',
+        linkedin: 'https://www.linkedin.com/in/alexandre-st-onge-060650203/',
+    }
+
+    
+];
--- data/execs/academicYearExecsMap.js
+++ data/execs/academicYearExecsMap.js
@@ -1,3 +1,4 @@
+import execs2024_2025 from './2024-2025.js';
 import execs2023_2024 from './2023-2024.js';
 import execs2022_2023 from './2022-2023.js';
 import execs2021_2022 from './2021-2022.js';
@@ -6,7 +7,8 @@ import execs2019_2020 from './2019-2020.js';
 
 // it's important for this to be more-recent-years=first
 const academicYearExecsMap = new Map();
-academicYearExecsMap.set('currentAcademicYear', '2023-2024');
+academicYearExecsMap.set('currentAcademicYear', '2024-2025');
+academicYearExecsMap.set('2024-2025', execs2024_2025);
 academicYearExecsMap.set('2023-2024', execs2023_2024);
 academicYearExecsMap.set('2022-2023', execs2022_2023);
 academicYearExecsMap.set('2021-2022', execs2021_2022);
