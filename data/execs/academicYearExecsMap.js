937fa4e600acf9b884fcea1c1818fc50e3b995c0
Negar-Badr <badr.negar@gmail.com>
2025-07-07 23:52:51 -0400

    Initial draft of 25-26

--- data/execs/academicYearExecsMap.js
+++ data/execs/academicYearExecsMap.js
@@ -1,3 +1,4 @@
+import execs2025_2026 from './2025-2026.js';
 import execs2024_2025 from './2024-2025.js';
 import execs2023_2024 from './2023-2024.js';
 import execs2022_2023 from './2022-2023.js';
@@ -7,7 +8,8 @@ import execs2019_2020 from './2019-2020.js';
 
 // it's important for this to be more-recent-years=first
 const academicYearExecsMap = new Map();
-academicYearExecsMap.set('currentAcademicYear', '2024-2025');
+academicYearExecsMap.set('currentAcademicYear', '2025-2026');
+academicYearExecsMap.set('2025-2026', execs2025_2026);
 academicYearExecsMap.set('2024-2025', execs2024_2025);
 academicYearExecsMap.set('2023-2024', execs2023_2024);
 academicYearExecsMap.set('2022-2023', execs2022_2023);
