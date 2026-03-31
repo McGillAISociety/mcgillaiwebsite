9d311769ab44d7f166f0dc9c810bfb7d26e4df84
Alexa <90080367+itsAlexa@users.noreply.github.com>
2026-03-31 00:28:50 -0400

    Revert "Exec Info Update (Alexa)"

--- data/execs/2024-2025.js
+++ data/execs/2024-2025.js
@@ -172,9 +172,9 @@ export default [
     },
     {
         photo: path('alexa'),
-        name: 'Alexa Vasilakos',
+        name: 'Alexa  Vasilakos',
         position: 'Senior Advisor',
-        bio: 'Alexa recently completed her Bachelor of Science in Software Engineering at McGill, and is starting work as an iOS Developer this fall. Her hobbies include swimming, drawing, and tending to her Animal Crossing island.',
+        bio: 'Alexa recently completed her Bachelor of Science in Software Engineering at McGill and is starting her first work term in the fall. Her hobbies include swimming competitively, drawing, and tending to her Animal Crossing island.',
         linkedin: 'https://www.linkedin.com/in/alexavas/',
     }
 
